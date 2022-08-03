e8168935c7023a8086ca85bb9666ecf90cdb4812
dotwoo <dotwoo@gmail.com>
2022-08-03 17:30:29 +0800

    fixed: 解决 AllowUsers 配置造成重启后,root ssh 连接失败

--- run.sh
+++ run.sh
@@ -1,3 +1,14 @@
 multipass launch -n primary --cloud-init cloud-config
 # multipass mount ~/dev primary:/home/dotwoo
 multipass list
+
+read -p "Do you want to shell primary? " yn
+if [[ "$yn" =~ "y" ]]; then
+    multipass shell primary
+fi
+
+read -p "Do you wish to delete primary? " yn
+if [[ "$yn" =~ "y" ]]; then
+    multipass delete primary
+    multipass purge
+fi
